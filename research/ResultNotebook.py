03bed1f589dfe1803994ad597de4e3614ce3e2d7
Uberariy <a3.okonishnikov@mail.ru>
2022-10-31 23:49:16 +0300

    Something happened on the day he died

---
+++ research/ResultNotebook.py
@@ -0,0 +1,101 @@
+"""
+This program is an .ipybn notebook utility, used to convert special data into excel.
+
+"""
+
+# %%
+'''Imports'''
+import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import math
+import os
+from collections import defaultdict
+
+# %%
+'''Functions cell'''
+def dict_to_df(d, progons):
+    s = pd.Series(d, index=pd.MultiIndex.from_tuples(d))
+    df = s.unstack()
+    return df.iloc[1:, 1:] / progons
+
+def draw_time_heatmap(path):
+    with open(path, 'r') as f:
+        text = f.read()
+    d = defaultdict(float)
+    for line in text.split("\n"):
+        procN = 0
+        workN = 0
+        time = 0
+        for elem in line.split(","):
+            if not elem:
+                break
+            name = elem.split(":")[0]
+            content = elem.split(":")[1]
+            if name == "Processors":
+                procN = int(content)
+            if name == "Works":
+                workN = int(content)
+            if name == "Duration":
+                time = float(content)
+        d[(procN, workN)] += time
+    plt.figure(figsize=(15, 15))
+    sns.heatmap(dict_to_df(d, 5), annot=True)
+    plt.title('Time of algorithm execution')
+    plt.xlabel('Processors Number')
+    plt.ylabel('Works Number')
+
+def draw_temperature_heatmap(path):
+    with open(path, 'r') as f:
+        text = f.read()
+    d = defaultdict(float)
+    d2 = defaultdict(float)
+    for line in text.split("\n"):
+        procN = 0
+        workN = 0
+        time = 0
+        law = ""
+        start_res = 0
+        final_res = 1
+        for elem in line.split(","):
+            if not elem:
+                break
+            name = elem.split(":")[0]
+            content = elem.split(":")[1]
+            if name == "Processors":
+                procN = int(content)
+            if name == "Works":
+                workN = int(content)
+            if name == "Duration":
+                time = float(content)
+            if name == "Law":
+                if int(content) == 0:
+                    law = "Boltzman Law"
+                elif int(content) == 1:
+                    law = "Cauchy Law"
+                elif int(content) == 2:
+                    law = "Natural Log Law"
+            if name == "StartCriterion":
+                start_res = int(content)
+            if name == "FinalCriterion":
+                final_res = int(content)
+        d[((procN, workN), law)] += time
+        d2[((procN, workN), law)] += start_res / final_res
+    print(dict_to_df(d2, 5))
+    plt.figure(figsize=(6, 6))
+    anno = dict_to_df(d2, 5)
+    print(anno)
+    sns.heatmap(dict_to_df(d, 5), annot=anno, fmt=".2%")
+    plt.title('Time period and quality (color presents time / percentage is quality upgrade)')
+    plt.xlabel('Law')
+    plt.ylabel('(Processors, Works) Number')
+
+
+# %%
+if __name__ == "__main__":
+    draw_time_heatmap("consequitivelog1")
+    draw_temperature_heatmap("consequitivelog2")
+
+# %%
